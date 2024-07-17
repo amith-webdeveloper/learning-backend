65d27a44e1a9ea2cee48dcdbb04688a702149901
amith-webdeveloper <amithwebdev1840@gmail.com>
2024-07-17 22:38:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const  express = require('express')
+const app = express()
+const port = process.env.PORT
+
+app.get('/' , (req , res)=>{
+    res.send('<h1> hi i am learning backend </h1>')
+})
+
+app.listen(port , function(){
+    console.log(`the app is listening at port ${port}`);
+    
+})
