4ef780d48fbbdc4ac2518c6903993971d78e536b
amith-webdeveloper <amithwebdev1840@gmail.com>
2024-07-26 23:10:13 +0530

    added day02 and day03

---
+++ data modelling day03/models/todos/user.models.js
@@ -0,0 +1,24 @@
+import mongoose, { model } from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+       username:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+       },
+       email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+       },
+       password:{
+        type: String,
+        required: true
+       }
+    },{timestamps : true}
+)
+
+export const User = mongoose.model('User', userSchema)
